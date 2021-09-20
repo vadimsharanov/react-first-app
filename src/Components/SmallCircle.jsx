f59669c11d38dba23305c1781f239a5f4df8df65
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-09-20 21:41:34 +0300

    done

--- src/Components/SmallCircle.jsx
+++ src/Components/SmallCircle.jsx
@@ -1,12 +1,13 @@
 function SmallCircle (props) {
         return (<div
-             style={{backgroundColor:props.color, borderRadius:props.figureType === "square" ? "0%" : "50%", margin:"40px"}} className="small-circle">
-                 
-                 <button onClick={props.delete} className="delete-button" >x</button>
-                 <button onClick={props.update}   >Update</button>
-                 <input onChange={props.updateInput} type="text"/>
+            style={{backgroundColor:props.color, borderRadius:props.figureType === "square" ? "0%" : "50%", margin:"10px"}} 
+            className="kruzhok"  >
+                 <button style={{width:"10%"}} onClick={props.deleteFigure} className="button-circle" >x</button>
+                 <button style={{width:"60%"}} onClick={props.updateFigure} className="button-circle" >Update</button>
+                 <input className="input-circle" onChange={props.updateFigureInput} type="text"/>
 
                  </div>) 
 
 }
+// style={{backgroundColor:"red", borderRadius:props.figureType === "square" ? "0%" : "50%", margin:"40px"}} className="small-circle"
 export default SmallCircle
