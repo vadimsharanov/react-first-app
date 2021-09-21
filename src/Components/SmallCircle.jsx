0737a60563de15ab48beff3eb09d2b3208e9ba56
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-09-21 10:19:15 +0300

    gap added

--- src/Components/SmallCircle.jsx
+++ src/Components/SmallCircle.jsx
@@ -1,6 +1,6 @@
 function SmallCircle (props) {
         return (<div
-            style={{backgroundColor:props.color, borderRadius:props.figureType === "square" ? "0%" : "50%", margin:"10px"}} 
+            style={{backgroundColor:props.color, borderRadius:props.figureType === "square" ? "0%" : "50%", margin:"30px"}} 
             className="kruzhok"  >
                  <button style={{width:"10%"}} onClick={props.deleteFigure} className="button-circle" >x</button>
                  <button style={{width:"60%"}} onClick={props.updateFigure} className="button-circle" >Update</button>
