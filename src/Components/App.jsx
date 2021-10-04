af4db6f402cd4514ebf4addd608af98ca811c372
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-10-04 13:44:37 +0300

    lets go

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,6 +5,7 @@ import Field from "./Field"
 const letters = ["L","b","g","w","m","u","Z"]
 
 export const themeContext = createContext({color: "black"})
+export const themeSelect = createContext({color: "black"})
 
 
 function App() {
@@ -13,16 +14,7 @@ function App() {
     })
 
     const changeTheme = (param) => {
-        console.log("vvv");
-        let theme
-        // const themes = {
-        //     1:"red",
-        //     2:"blue",
-        //     3:"yellow"
-        // }
-        // return themes[theme]
-        
-
+        let theme;
         if ( param ===1 ) {
             theme = {
                 color:"red"
@@ -43,13 +35,14 @@ function App() {
             setStyle(theme)
         }
     }
-    console.log(style);
     return (<>
-    <themeContext.Provider value={style}>
+    <themeContext.Provider >
+    <themeSelect.Provider value={style}>
         <Field letters={letters} ></Field>
         <button onClick={() => changeTheme(1)} className="baton" >Theme 1 </button>
         <button onClick={() => changeTheme(2)} className="baton" >Theme 2 </button>
         <button onClick={() => changeTheme(3)} className="baton" >Theme 3 </button>
+    </themeSelect.Provider>
     </themeContext.Provider>
      
     </>)
--- src/Components/Letter.jsx
+++ src/Components/Letter.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react"
-import { themeContext } from "./App.jsx"
+import { themeContext, themeSelect } from "./App.jsx"
+
 
 function Letter({letter}) {
     const th = useContext(themeContext)
+    const ts = useContext(themeSelect)
     return (
     <>
         <span style={th} className="letter">{letter} </span>
